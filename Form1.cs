c67671697aa57f7e79d53364cd06353b394a98a1
Rui Carvalho <iscac15784@alumni.iscac.pt>
2019-04-11 15:39:12 +0100

    poi

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,18 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+
+        public int MyProperty { get; set; }
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@ namespace WindowsFormsApp2
 {
     static class Program
     {
+
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
+
+    }
     }
 }
